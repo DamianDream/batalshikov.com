3d9b5b93638a59f9f7cb84d42737ecb13733950f
Dima Batalshikov <batalshikov.d@gmail.com>
2025-11-11 01:04:42 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div id="intro">
+        <h1>Hi there!</h1>
+        <p>The page is under construction, you can check back soon.</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
